b405ba98dbcbd53e64e50d41a9b7f4483ad870f5
Osho <arnav.saraf777@gmail.com>
2016-07-14 18:02:31 +0530

    Update weathero.js

--- scripts/weathero.js
+++ scripts/weathero.js
@@ -1,16 +1,13 @@
 // Javascript and JQuery here
 $(document).ready(function(){
 //Variables here
-	var urlOWM;
-	var urlGMA
-	var userCity;
-	var weatherIconClass = "wi-owm-";
-	var tempWeatherIconClass = weatherIconClass;
-	var celsius;
+	var forecastIconClass = "wi-forecast-io-";
+	var tempForecastIconClass = forecastIconClass;
+	var tempCelsius;
 	var currentTimeUnix;
 	var currentTime;
-	var timestamp;
-	var timezone;
+	var lat;
+	var lon;
 
 // Check if local or global
 	$("#city").keyup(function(){
@@ -27,12 +24,9 @@ $(document).ready(function(){
 		$(document).ajaxStart(function(){
 			$(".contents").hide();
 			$(".contents-loading").show();
+			$("#forecastIcon").removeClass(forecastIconClass);
+			forecastIconClass = tempForecastIconClass;
 			});
-		$(document).ajaxComplete(function(){
-			$(".contents-loading").hide();
-			$(".contents").show();
-			});
-		$.ajaxSetup({async:false});
 		if($("#city").val() !== ""){
 			globalWeatherFunction();
 			}
@@ -43,58 +37,36 @@ $(document).ready(function(){
 // Local Weather Function
 	function localWeatherFunction(){
 		var time = moment();
-		var hour = time.format("HH");
 		dateString = time.format("MMM Do h:mm a");
-		$("#weatherIcon").removeClass(weatherIconClass);
-		weatherIconClass = tempWeatherIconClass;
-		if(hour > 6 && hour < 21){
-			weatherIconClass += "day-";
-			}
-		else{
-			weatherIconClass += "night-";	
-			}
-		$.getJSON("https://freegeoip.net/json/",status,function(data){
-			lat = data.latitude;
-			lon = data.longitude;
-			});
-		urlOWM = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=f0deccd8666bd85749986f9353b61001";
-		$.getJSON(urlOWM,status,function(data){
-			weatherIconClass += data.weather[0].id;
-			celsius = Math.floor(data.main.temp - 273.15);
-			$("#weatherIcon").addClass(weatherIconClass);
-			$(".temp").html(celsius + "<i class=\"wi wi-celsius\">");
-			$(".date").html(dateString);
-			$(".wi-wind").addClass("towards-"+Math.floor(data.wind.deg)+"-deg");
-			$(".speed").html(Math.floor(data.wind.speed)+"m/s");
+		$.ajax({
+			url:"https://freegeoip.net/json/",
+			dataType:"json",
+			success: function(data){
+				lat = data.latitude;
+				lon = data.longitude;
+				$.ajax({
+					url: "http://cors.io/?u=https://api.forecast.io/forecast/938ea9c32b67be8a23ed0c908a4368a8/"+lat+","+lon,
+					dataType:"json",
+					success: function(data_1){
+						forecastIconClass += data_1.currently.icon;
+						tempCelsius = data_1.currently.temperature;
+						windBearing = data_1.currently.windBearing;
+						windSpeed = data_1.currently.windSpeed;
+						},
+					complete: function(){
+						$(".contents-loading").hide();
+						$(".contents").show();
+						$("#forecastIcon").addClass(forecastIconClass);
+						$(".temp").html(Math.floor(tempCelsius) + "<i class=\"wi wi-celsius\">");
+						$(".date").html(dateString);
+						$(".wi-wind").addClass("towards-"+Math.floor(windBearing)+"-deg");
+						$(".speed").html(Math.floor(windSpeed)+"m/s");					
+						},
+					});
+				},
 			});
 		}
 // Global Weather Function
-	function globalWeatherFunction(){
-		timestamp = 0;
-		$("#weatherIcon").removeClass(weatherIconClass);
-		weatherIconClass = tempWeatherIconClass;
-		userCity = $("#city").val();
-		urlOWM = "https://api.openweathermap.org/data/2.5/weather?q="+userCity+"&APPID=f0deccd8666bd85749986f9353b61001";
-		$.getJSON(urlOWM,status,function(data){
-			urlGMA = "https://maps.googleapis.com/maps/api/timezone/json?location="+data.coord.lat+","+data.coord.lon+"&timestamp="+timestamp+"&key=AIzaSyDhay8OvZIBWL-uoedWMapcfH3J7DLvxkM";
-			$.getJSON(urlGMA,status,function(result){
-				timezone = result.timeZoneId;
-				});
-			currentTime = moment().tz(timezone);
-			if(currentTime.format("HH") > 6 && currentTime.format("HH") < 21){
-				weatherIconClass += "day-";
-				}
-			else{
-				weatherIconClass += "night-";	
-				}
-			weatherIconClass += data.weather[0].id;
-			celsius = Math.floor(data.main.temp - 273.15);
-			$("#weatherIcon").addClass(weatherIconClass);
-			$(".temp").html(celsius + "<i class=\"wi wi-celsius\">");
-			$(".date").html(currentTime.format("MMM Do YYYY hh:mm a"));
-			$(".wi-wind").addClass("towards-"+Math.floor(data.wind.deg)+"-deg");
-			$(".speed").html(Math.floor(data.wind.speed)+"m/s");
-			});
-		}
 	});
 
+// urlF = "https://api.forecast.io/forecast/938ea9c32b67be8a23ed0c908a4368a8/"+lat+","+lon+"?exclude=[currently, minutely, hourly, daily, alerts, flags]&units=si";
