e90339c896048892e6f5b6dee199fcf20b37bbe7
Osho <arnav.saraf777@gmail.com>
2016-07-15 13:02:05 +0530

    Update weathero.js

--- scripts/weathero.js
+++ scripts/weathero.js
@@ -4,10 +4,13 @@ $(document).ready(function(){
 	var lat;
 	var lon;
 	var urlOWM;
+	var urlGMA;
 	var temperature; 
 	var windSpeed;
 	var windDeg;
 	var convertedTemp;
+	var timezone;
+	var requestedTimezoneTime;
 	var weatherIconClass = "wi-owm-";
 	var tempWeatherIconClass = weatherIconClass;
 // Field blank or not
@@ -24,8 +27,6 @@ $(document).ready(function(){
 	$("#enterButton").click(function(){
 		$.ajaxSetup({async:false});
 		$(document).ajaxStart(function(){
-			$("#weatherIcon").removeClass(weatherIconClass);
-			$(".wi-wind").removeClass("wi-wind.towards-"+windDeg+"-deg");
 			$(".contents").hide();
 			$(".contents-loading").show();
 		});
@@ -42,6 +43,8 @@ $(document).ready(function(){
 	});
 	// Local weather function 
 	function localWeatherFunction(){
+		$("#weatherIcon").removeClass(weatherIconClass);
+		$(".wi-wind").removeClass("wi-wind.towards-"+windDeg+"-deg");
 		weatherIconClass = tempWeatherIconClass;
 		var time = moment();
 		if(time.format("HH") < 20 && time.format("HH") > 6){
@@ -59,7 +62,6 @@ $(document).ready(function(){
 			temperature = Math.floor(convert(data.main.temp,"kelvin","celsius"));
 			windSpeed = Math.floor(data.wind.speed);
 			windDeg = Math.floor(data.wind.deg);
-			alert(weatherIconClass);
 			$("#weatherIcon").addClass(weatherIconClass);
 			$(".temp").html(temperature + "<i class = \"wi wi-celsius\"></i>");
 			$(".date").html(time.format("MMM Do hh:mm a"));
@@ -69,6 +71,32 @@ $(document).ready(function(){
 	}
 // Global weather function
 	function globalWeatherFunction(){
+		$("#weatherIcon").removeClass(weatherIconClass);
+		$(".wi-wind").removeClass("wi-wind.towards-"+windDeg+"-deg");
+		weatherIconClass = tempWeatherIconClass;
+		urlOWM = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?q="+$("#city").val()+"&APPID=f0deccd8666bd85749986f9353b61001"; 
+		$.getJSON(urlOWM, function(data){
+			urlGMA = "https://maps.googleapis.com/maps/api/timezone/json?location="+data.coord.lat+","+data.coord.lon+"&timestamp=0&key=AIzaSyDhay8OvZIBWL-uoedWMapcfH3J7DLvxkM";
+			$.getJSON(urlGMA,status,function(result){
+				timezone = result.timeZoneId;
+				});
+			requestedTimezoneTime = moment().tz(timezone);
+			if(requestedTimezoneTime.format("HH") > 6 && requestedTimezoneTime.format("HH") < 21){
+				weatherIconClass += "day-";
+				}
+			else{
+				weatherIconClass += "night-";	
+				}			
+			weatherIconClass += data.weather[0].id;
+			temperature = Math.floor(convert(data.main.temp,"kelvin","celsius"));
+			windSpeed = Math.floor(data.wind.speed);
+			windDeg = Math.floor(data.wind.deg);
+			$("#weatherIcon").addClass(weatherIconClass);
+			$(".temp").html(temperature + "<i class = \"wi wi-celsius\"></i>");
+			$(".date").html(requestedTimezoneTime.format("MMM Do hh:mm a"));
+			$(".speed").html(windSpeed + "m/s");
+			$(".wi-wind").addClass("towards-"+Math.floor(data.wind.deg)+"-deg");
+		});
 		
 	}
 // Convert between celsius, kelvin and fahrenheit
