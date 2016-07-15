efeaeec1508db58baf61bada2bca6746d38a3f8d
Osho <arnav.saraf777@gmail.com>
2016-07-15 12:23:42 +0530

    Update weathero.js

--- scripts/weathero.js
+++ scripts/weathero.js
@@ -1,72 +1,104 @@
-// Javascript and JQuery here
+// Javascript and jQuery here
 $(document).ready(function(){
-//Variables here
-	var forecastIconClass = "wi-owm-day-";
-	var tempForecastIconClass = forecastIconClass;
-	var tempCelsius;
-	var currentTimeUnix;
-	var currentTime;
+// Variables here
 	var lat;
 	var lon;
-
-// Check if local or global
+	var urlOWM;
+	var temperature; 
+	var windSpeed;
+	var windDeg;
+	var convertedTemp;
+	var weatherIconClass = "wi-owm-";
+	var tempWeatherIconClass = weatherIconClass;
+// Field blank or not
 	$("#city").keyup(function(){
-		if($("#city").val() !== ""){
-			$("#enterButton").html("Search");
-			}
-		else{
+		if($("#city").val() === ""){
 			$("#enterButton").html("For Your City");
-			}
-		});
-				
-// If Button is pressed
-	$("#enterButton").click(function(){
-		$(document).ajaxStart(function(){
-			$(".contents").hide();
-			$(".contents-loading").show();
-			$("#forecastIcon").removeClass(forecastIconClass);
-			forecastIconClass = tempForecastIconClass;
-			});
-		if($("#city").val() !== ""){
-			globalWeatherFunction();
-			}
-		else{
-			localWeatherFunction();
-			}
-		});
-// Local Weather Function
-	function localWeatherFunction(){
-		var time = moment();
-		dateString = time.format("MMM Do h:mm a");
-		$.ajax({
-			url:"https://freegeoip.net/json/",
-			dataType:"json",
-			success: function(data){
-				lat = data.latitude;
-				lon = data.longitude;
-				$.ajax({
-					url: "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?q=jalgaon&APPID=f0deccd8666bd85749986f9353b61001",
-					dataType:"json",
-					success: function(data_1){
-						forecastIconClass += data_1.weather[0].id;
-						tempCelsius = data_1.main.temp;
-						windBearing = data_1.wind.deg;
-						windSpeed = data_1.wind.speed;
-						},
-					complete: function(){
-						$(".contents-loading").hide();
-						$(".contents").show();
-						$("#forecastIcon").addClass(forecastIconClass);
-						$(".temp").html(Math.floor(tempCelsius) + "<i class=\"wi wi-celsius\">");
-						$(".date").html(dateString);
-						$(".wi-wind").addClass("towards-"+Math.floor(windBearing)+"-deg");
-						$(".speed").html(Math.floor(windSpeed)+"m/s");					
-						},
-					});
-				},
-			});
 		}
-// Global Weather Function
+		else{
+			$("#enterButton").html("Search");			
+		}
 	});
 
-// urlF = "https://api.forecast.io/forecast/938ea9c32b67be8a23ed0c908a4368a8/"+lat+","+lon+"?exclude=[currently, minutely, hourly, daily, alerts, flags]&units=si";
+// Go to function  
+	$("#enterButton").click(function(){
+		$.ajaxSetup({async:false});
+		$(document).ajaxStart(function(){
+			$("#weatherIcon").removeClass(weatherIconClass);
+			$(".wi-wind").removeClass("wi-wind.towards-"+windDeg+"-deg");
+			$(".contents").hide();
+			$(".contents-loading").show();
+		});
+		$(document).ajaxComplete(function(){
+			$(".contents-loading").hide();
+			$(".contents").show();
+		});
+		if($("#city").val() === ""){
+			localWeatherFunction();
+		}
+		else{
+			globalWeatherFunction();
+		}
+	});
+	// Local weather function 
+	function localWeatherFunction(){
+		weatherIconClass = tempWeatherIconClass;
+		var time = moment();
+		if(time.format("HH") < 20 && time.format("HH") > 6){
+			weatherIconClass += "day-";
+		}else{
+			weatherIconClass += "night-";
+		}
+		$.getJSON("https://freegeoip.net/json/",function(data){
+			lat = data.latitude;
+			lon = data.longitude;
+		});
+		urlOWM = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=f0deccd8666bd85749986f9353b61001"; 
+		$.getJSON(urlOWM, function(data){
+			weatherIconClass += data.weather[0].id;
+			temperature = Math.floor(convert(data.main.temp,"kelvin","celsius"));
+			windSpeed = Math.floor(data.wind.speed);
+			windDeg = Math.floor(data.wind.deg);
+			$("#weatherIcon").addClass(weatherIconClass);
+			$(".temp").html(temperature + "<i class = \"wi wi-celsius\"></i>");
+			$(".date").html(time.format("MMM Do hh:mm a"));
+			$(".speed").html(windSpeed + "m/s");
+			$(".wi-wind").addClass("towards-"+Math.floor(data.wind.deg)+"-deg");
+		});
+	}
+// Global weather function
+	function globalWeatherFunction(){
+		
+	}
+// Convert between celsius, kelvin and fahrenheit
+	function convert(temp, tempUnit, conversionUnit){
+		switch(tempUnit){
+			case "kelvin":
+				if(conversionUnit === "celsius"){
+					convertedTemp = temp - 273.15;
+				}
+				else if(conversionUnit === "fahrenheit"){
+					convertedTemp = (temp*9/5) - 459.67;
+				}
+				break;
+			case "celsius":
+				if(conversionUnit === "kelvin"){
+					convertedTemp = temp + 273.15
+				}
+				else if(conversionUnit === "fahrenheit"){
+					convertedTemp = (temp*9/5) + 32;
+				}
+				break;
+			case "fahrenheit":
+				if(conversionUnit === "kelvin"){
+					convertedTemp = (temp + 459.67)*5/9;
+				}
+				else if(conversionUnit === "celsius"){
+					convertedTemp = (temp -32)*5/9;
+				}
+				break;
+		}
+		return convertedTemp;
+	}
+});
+
